428bdc692843bded45c684df866f844ad95bbee3
v4n1tzy <166169684+v4n1tzy@users.noreply.github.com>
2025-03-22 03:15:48 +0100

    Update front.js

--- front.js
+++ front.js
@@ -1,6 +1,6 @@
 // Modifier la fonction pour récupérer l'email depuis le backend Flask
 function generateNewEmail() {
-    fetch('https://ton-url-deployee/render.com/generate_email')
+    fetch('https://tempmaila.netlify.app/generate_email')
         .then(response => response.json())
         .then(data => {
             document.getElementById("email").value = data.email;
